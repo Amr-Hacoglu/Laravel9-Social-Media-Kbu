21d9942860d7bff111c79e7abf073699687911c6
Amr-Hacoglu <omer.abdulw221@gmail.com>
2022-04-08 13:13:56 +0300

    New Change

---
+++ resources/views/home/test3.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a test page 2 </h1>
+Id value : {{$id}}
+<br>
+Number value : {{$number}}
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -12,8 +13,42 @@ use Inertia\Inertia;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-| new
+|
 */
+
+// 1- do something on the route
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+// 2- call view in the route
+// the doc. say instead of policy - welcome but i didn't have welcome page
+
+Route::get('/policy', function () {
+    return view('policy');
+});
+
+// 3- Call controller function
+// php artisan make:controller HomeController
+
+Route::get('/ind',[HomeController::class,'index'])->name('home'); // here we import the class upper to not be tall text
+// here we creat a new page and inside the page we create a function and here we call the function
+
+// 4- Route - Controller - View
+
+Route::get('/test1',[HomeController::class,'test'])->name('test');
+
+// 5- Route with parameters
+
+Route::get('/param/{id}/{number}',[HomeController::class,'param'])->name('param');
+
+// 6- Route with post
+
+Route::post('/save',[HomeController::class,'save'])->name('save'); // post instead of get
+
+
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
